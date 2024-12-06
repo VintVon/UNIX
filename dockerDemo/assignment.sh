ced89ecffdda825631b221be0ea171a3726a2155
Vito <nubasian0101@gmail.com>
2024-12-06 04:05:15 +0000

    Docker Assignment 7

--- dockerDemo/assignment.sh
+++ dockerDemo/assignment.sh
@@ -1,1 +1,9 @@
-echo Viet Vo
+#!/bin/bash
+apt update
+apt install -y nginx
+apt install -y vim
+service nginx start
+
+sed -i 's/Welcome to nginx/Welcome to Viet Vos page/' /var/www/html/*.html
+
+sed -i 's/nginx/nginx (pronounced as EngineX)/' /var/www/html/*.html
